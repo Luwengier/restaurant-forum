ca3aa660aee947d75d4790ddf34540eee2e06aa5
Luwengier <g217881111@gmail.com>
2019-11-01 16:00:09 +0800

    Add user model and register features

--- app.js
+++ app.js
@@ -2,10 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const db = require('./models')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-app.listen(3000, () => {
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+
+app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Example app listening on port ${port}!`)
 })
+
+// setting routes
+require('./routes')(app)
+
