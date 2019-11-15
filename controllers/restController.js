d4fb95594d8cc47afa8e63b9e0415a7f808e7a37
Luwengier <g217881111@gmail.com>
2019-11-15 13:28:37 +0800

    Renew deserializeUser in passport.js,and add favorited feature in restaurant routes

--- controllers/restController.js
+++ controllers/restController.js
@@ -28,7 +28,8 @@ let restController = {
       // clean up restaurant data
       const data = result.rows.map(r => ({
         ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50)
+        description: r.dataValues.description.substring(0, 50),
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll().then(categories => {
         return res.render('restaurants', {
@@ -47,14 +48,17 @@ let restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
     }).then(restaurant => {
       return restaurant.increment({
         'viewCounts': 1
       }).then((restaurant) => {
+        const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
         return res.render('restaurant', {
-          restaurant: restaurant
+          restaurant: restaurant,
+          isFavorited: isFavorited
         })
       })
     })
