7164aad6c9ef9c7dbf678a015bfa70cba2b40bd5
Luwengier <g217881111@gmail.com>
2019-11-16 20:11:38 +0800

    Add topuser feature

--- routes/index.js
+++ routes/index.js
@@ -64,6 +64,9 @@ module.exports = (app, passport) => {
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
+  // Top user
+  app.get('/users/top', authenticated, userController.getTopUser)
+
   // Profile
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
